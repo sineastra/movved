742abf2faff5977ab4119e1a1c8fcf2b91c90dc4
Sineastra <lord.of.light.0002@gmail.com>
2022-01-30 22:02:59 +0200

    added mobile header tests and structure

---
+++ src/Components/Header/MobileHeader/MobileHeader.test.tsx
@@ -0,0 +1,37 @@
+import { BrowserRouter } from "react-router-dom"
+import { render, screen } from "@testing-library/react"
+import MobileHeader from "./MobileHeader"
+import { MouseEvent } from "react"
+
+
+const mockedToggleAsideNav = (e: MouseEvent) => {
+	return undefined
+}
+
+describe("Testing Mobile Header", () => {
+	const renderMobileHeader = () => {
+		render(
+			<BrowserRouter>
+				<MobileHeader toggleAsideNav={ mockedToggleAsideNav }/>
+			</BrowserRouter>,
+		)
+	}
+	it("site logo should be present", () => {
+		renderMobileHeader()
+		const logo = screen.getByAltText('main-logo')
+
+		expect(logo).toBeInTheDocument()
+	})
+	it("Bars for toggle side menu should be present", () => {
+		renderMobileHeader()
+		const sideBars = screen.getByTitle(/open side menu/i)
+
+		expect(sideBars).toBeInTheDocument()
+	})
+	it("search input should be present", () => {
+		renderMobileHeader()
+		const searchInput = screen.getByRole('searchbox')
+
+		expect(searchInput).toBeInTheDocument()
+	})
+})
--- src/Components/Header/MobileHeader/MobileHeader.tsx
+++ src/Components/Header/MobileHeader/MobileHeader.tsx
@@ -1,14 +1,20 @@
 import { mobilePropsInterface } from "../_misc/_interfaces"
 import styles from "../Header.module.scss"
 import { FaBars } from "react-icons/fa"
+import mainLogo from "../../../assets/main-logo.svg"
+import SearchInput from "../../SearchInput/SearchInput"
 
 
 const MobileHeader = ({ toggleAsideNav, className = '' }: mobilePropsInterface) => {
 
 	return (
 		<div className={ className }>
+			<div className={ styles.mainLogoWrapper }>
+				<img src={ mainLogo } alt="main-logo"/>
+			</div>
+			<SearchInput pathname={ "/" }/>
 			<div className={ styles.showSideMenuBtn } onClick={ toggleAsideNav }>
-				<FaBars/>
+				<FaBars title="Open Side Menu"/>
 			</div>
 		</div>
 	)
